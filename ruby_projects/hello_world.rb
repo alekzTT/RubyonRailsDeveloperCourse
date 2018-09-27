e59c4b82323aed9dd29be9b83496f448fbc4de2e
alekzTT <alekz.th@gmail.com>
2018-09-27 22:15:26 +0000

    initial commit

---
+++ ruby_projects/hello_world.rb
@@ -0,0 +1,23 @@
+puts "Hello World"
+puts("puts")
+
+
+
+#vars
+hello ="Hello Ruby Dev!! "
+puts hello
+
+#method / function 
+def speak
+    puts "hello Method"
+end
+
+speak
+
+
+#method / function  with arguments
+def speak(name)
+    puts "HELLO "+name
+end
+
+speak "ALEX"
