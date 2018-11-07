61c1c6f722fbdd8adb3c69d48f3f17dd0226b7a6
alekzTT <alekz.th@gmail.com>
2018-11-07 10:12:50 +0000

    create, add flash error messages, create show

--- rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -9,10 +9,18 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    #if the article is saved (validation passing e.t.c.)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
   end
   
+  def show
+   @article = Article.find(params[:id])
+  end
   
   private
   
