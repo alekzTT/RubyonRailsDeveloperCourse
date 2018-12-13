0d1e7edb0f3f04d9b1fcd21c71a3bccf611dfe27
alekzTT <alekz.th@gmail.com>
2018-12-13 09:46:46 +0000

    Depot Scaffold scss and Layout

--- rails_projects/depot/db/seeds.rb
+++ rails_projects/depot/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+#....
+5.times {
+  Product.create!(title: 'Ruby on Rails', 
+  description: %{<p>
+    Adding Ruby to ToolBox Now we have to type more than 80 chars in order to see what the truncate function does are these more than 80 chars ?
+  </p>}, 
+   image_url: 'ruby.jpg',
+   price: 45.54)
+   
+Product.create!(title: 'Javascript', 
+  description: %{<p>2ndn2nd Adding Ruby to ToolBox</p>}, 
+   image_url: 'ruby.jpg',
+   price: 45.54)
+}
